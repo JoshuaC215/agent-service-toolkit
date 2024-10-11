879dfcb355de6fc56b4504b1ab1a02242e824e82
gbaian10 <34255899+gbaian10@users.noreply.github.com>
2024-10-12 02:40:01 +0800

    feat: Add dev mode (#55)

--- src/run_service.py
+++ src/run_service.py
@@ -1,9 +1,14 @@
+import os
+
 import uvicorn
 from dotenv import load_dotenv
 
 load_dotenv()
 
 if __name__ == "__main__":
-    from service import app
+    if os.getenv("MODE") != "dev":
+        from service import app
 
-    uvicorn.run(app, host="0.0.0.0", port=80)
+        uvicorn.run(app, host="0.0.0.0", port=80)
+    else:
+        uvicorn.run("service:app", reload=True)
