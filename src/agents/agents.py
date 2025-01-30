3773ea09956ffdf69b9fa7927d7fa3f7f0dec4d7
Joshua Carroll <carroll.joshk@gmail.com>
2025-01-29 20:39:39 -0800

    Initial fix for command support

--- src/agents/agents.py
+++ src/agents/agents.py
@@ -4,6 +4,7 @@ from langgraph.graph.state import CompiledStateGraph
 
 from agents.bg_task_agent.bg_task_agent import bg_task_agent
 from agents.chatbot import chatbot
+from agents.command_agent import command_agent
 from agents.research_assistant import research_assistant
 from schema import AgentInfo
 
@@ -21,6 +22,7 @@ agents: dict[str, Agent] = {
     "research-assistant": Agent(
         description="A research assistant with web search and calculator.", graph=research_assistant
     ),
+    "command-agent": Agent(description="A command agent.", graph=command_agent),
     "bg-task-agent": Agent(description="A background task agent.", graph=bg_task_agent),
 }
 
---
+++ src/agents/command_agent.py
@@ -0,0 +1,54 @@
+import random
+from typing import Literal
+
+from langchain_core.messages import AIMessage
+from langgraph.graph import START, MessagesState, StateGraph
+from langgraph.types import Command
+
+
+class AgentState(MessagesState, total=False):
+    """`total=False` is PEP589 specs.
+
+    documentation: https://typing.readthedocs.io/en/latest/spec/typeddict.html#totality
+    """
+
+
+# Define the nodes
+
+
+def node_a(state: AgentState) -> Command[Literal["node_b", "node_c"]]:
+    print("Called A")
+    value = random.choice(["a", "b"])
+    # this is a replacement for a conditional edge function
+    if value == "a":
+        goto = "node_b"
+    else:
+        goto = "node_c"
+
+    # note how Command allows you to BOTH update the graph state AND route to the next node
+    return Command(
+        # this is the state update
+        update={"messages": [AIMessage(content=f"Hello {value}")]},
+        # this is a replacement for an edge
+        goto=goto,
+    )
+
+
+def node_b(state: AgentState):
+    print("Called B")
+    return {"messages": [AIMessage(content="Hello B")]}
+
+
+def node_c(state: AgentState):
+    print("Called C")
+    return {"messages": [AIMessage(content="Hello C")]}
+
+
+builder = StateGraph(AgentState)
+builder.add_edge(START, "node_a")
+builder.add_node(node_a)
+builder.add_node(node_b)
+builder.add_node(node_c)
+# NOTE: there are no edges between nodes A, B and C!
+
+command_agent = builder.compile()
