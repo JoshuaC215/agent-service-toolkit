28a131db18c1ff58d3f881252365b7eb28865ac6
Thorbjorg Petursdottir <TP@team-consulting.com>
2025-02-18 13:55:49 +0000

    Issue #141 - Add RAG assistant template

--- src/agents/agents.py
+++ src/agents/agents.py
@@ -5,6 +5,7 @@ from langgraph.graph.state import CompiledStateGraph
 from agents.bg_task_agent.bg_task_agent import bg_task_agent
 from agents.chatbot import chatbot
 from agents.command_agent import command_agent
+from agents.rag_assistant import rag_assistant
 from agents.research_assistant import research_assistant
 from schema import AgentInfo
 
@@ -21,6 +22,9 @@ agents: dict[str, Agent] = {
     "chatbot": Agent(description="A simple chatbot.", graph=chatbot),
     "research-assistant": Agent(
         description="A research assistant with web search and calculator.", graph=research_assistant
+    ),
+    "rag-assistant": Agent(
+        description="A RAG assistant with access to information in a database.", graph=rag_assistant
     ),
     "command-agent": Agent(description="A command agent.", graph=command_agent),
     "bg-task-agent": Agent(description="A background task agent.", graph=bg_task_agent),
