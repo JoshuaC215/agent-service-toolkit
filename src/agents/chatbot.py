3e1c8ad22bd6779a14a098e9f74154545246fa6d
Maciek Bryński <maciek@brynski.pl>
2025-04-15 14:51:00 +0200

    Fix chatbot

--- src/agents/chatbot.py
+++ src/agents/chatbot.py
@@ -2,7 +2,6 @@ from langchain_core.messages import BaseMessage
 from langchain_core.runnables import RunnableConfig
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.func import entrypoint
-from langgraph.graph import add_messages
 
 from core import get_model, settings
 
@@ -16,10 +15,10 @@ async def chatbot(
 ):
     messages = inputs["messages"]
     if previous:
-        messages = add_messages(previous["messages"], messages)
+        messages = previous["messages"] + messages
 
     model = get_model(config["configurable"].get("model", settings.DEFAULT_MODEL))
     response = await model.ainvoke(messages)
     return entrypoint.final(
-        value={"messages": [response]}, save={"messages": add_messages(messages, response)}
+        value={"messages": [response]}, save={"messages": messages + [response]}
     )
